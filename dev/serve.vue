9f8967ccb7ec3faf0a5f2c6d969164efa7613e32
Jason Anger <jayandn1999@gmail.com>
2022-01-22 15:36:56 +0700

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import VueAlert from '@/vue-alert.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    VueAlert
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-alert />
+  </div>
+</template>
